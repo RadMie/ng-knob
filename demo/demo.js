cc2d8929964f75335d3257f9c823318df962f664
Radoslaw Mietkiewicz <radoslaw.mietkiewicz@gmail.com>
2015-12-11 01:06:35 +0100

    update demo

--- demo/demo.js
+++ demo/demo.js
@@ -1,10 +1,10 @@
 'use strict';
 
 angular.module('KnobDemoApp', ['ui.knob'])
-  .controller('knobCtrl', function ($scope) {
+  .controller('knobCtrl1', function ($scope) {
     $scope.value = 65;
     $scope.options = {
-      skin: 'simple',               //(default: "simple")
+      skin: 'tron',               //(default: "simple")
       animate: {
         enabled: true,              //(default: true)
         duration: 1000,             //(default: 1000)
@@ -13,18 +13,31 @@ angular.module('KnobDemoApp', ['ui.knob'])
       size: 200,                    //(default: "200")
       startAngle: 0,                //(default: "0")
       endAngle: 360,                //(default: "360")
-      offsetAngle: 0,               //(default: "0")
-      readOnly: false,              //(default: "false")
-      fgColor: '#543',              //(default: "#")
-      bgColor: '#345',              //(default: "#")
-      textColor: '#000',            //(default: "#")
-      thickness: 50,                //(default: "50")
+      unit: "%",                    //(default: "false")
       displayInput: true,           //(default: "true")
-      unit: false,                  //(default: "false")
-      step: 1,                      //(default: "1")
-      min: 0,                       //(default: "0")
-      max: 100,                     //(default: "100")
-      lineCap: 'simple',            //(default: "simple")
-      displayPrevious: true,        //(default: "true")
+      readOnly: false,              //(default: "false")
+      trackWidth: 50,
+      barWidth: 40,
+      trackColor: "rgba(255,0,0,.1)",
+      barColor: "rgba(255,0,0,.5)",
+      prevBarColor: "rgba(0,0,0,.2)",
+      textColor: '#222',            //(default: "#")
+      barCap: 0,
+
+      //offsetAngle: 0,               //(default: "0")
+      //step: 1,                      //(default: "1")
+      //min: 0,                       //(default: "0")
+      //max: 100,                     //(default: "100")
+      //displayPrevious: true,        //(default: "true")
+      //scaleColor
+      //scaleLength
+    };
+  })
+  .controller('knobCtrl2', function ($scope) {
+    $scope.value = 50;
+    $scope.options = {
+      skin: "simple",
+      trackWidth: 50,
+      barWidth: 50
     };
   });
