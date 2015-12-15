55f83fdcf46414e44f8f9599f06a2ea57084391d
Radoslaw Mietkiewicz <radoslaw.mietkiewicz@gmail.com>
2015-12-15 19:24:03 +0100

    update demo

--- demo/demo.js
+++ demo/demo.js
@@ -4,44 +4,142 @@ angular.module('KnobDemoApp', ['ui.knob'])
   .controller('knobCtrl1', function ($scope) {
     $scope.value = 65;
     $scope.options = {
-      skin: 'tron',                       //(default: 'simple')
-      animate: {
-        enabled: true,                    //(default: true)
-        duration: 1000,                   //(default: 1000)
-        ease: 'bounce'                    //(default: 'bounce')
+      skin: {
+        type: 'tron'
       },
-      size: 300,                          //(default: 200)
-      startAngle: 0,                      //(default: 0)
-      endAngle: 360,                      //(default: 360)
-      unit: "%",                          //(default: '')
-      displayInput: true,                 //(default: true)
-      readOnly: false,                    //(default: fasle)
-      trackWidth: 50,                     //(default: 50)
-      barWidth: 40,                       //(default: 50)
-      trackColor: 'rgba(255,0,0,.1)',     //(default: 'rgba(255,0,0,.1)')
-      barColor: 'rgba(255,0,0,.5)',       //(default: 'rgba(255,0,0,.5)')
-      prevBarColor: 'rgba(0,0,0,.2)',     //(default: 'rgba(0,0,0,.2)')
-      textColor: '#222',                  //(default: '#222')
-      barCap: 0,                          //(default: 0)
-      fontSize: 'auto',                   //(default: 'auto')
+      size: 300,
+      unit: "%",
+      barWidth: 40,
+      trackColor: 'rgba(255,0,0,.1)',
+      prevBarColor: 'rgba(0,0,0,.2)',
       subText: {
-        enabled: true,                    //(default: false)
-        text: 'CPU used',                 //(default: '')
-        color: 'gray',                    //(default: 'gray')
-        font: 'auto'                      //(default: 'auto')
+        enabled: true,
+        text: 'CPU used'
       },
-      bgColor: '',                        //(default: '')
       scale: {
-        enabled: true,                    //(default: false)
-        type: 'lines',                    //(default: 'lines')
-        color: 'gray',                    //(default: 'gray')
-        width: 3,                         //(default: 4)
-        quantity: 20,                     //(default: 20)
-        height: 10                        //(default: 10)
+        enabled: true,
+        type: 'lines',
+        width: 3
       },
-      step: 5,                            //(default: 1)
-      displayPrevious: true,              //(default: true)
-      min: -100,                          //(default: 0)
-      max: 100                            //(default: 100)
+      step: 5,
+      displayPrevious: true
     };
+  })
+  .controller('knobCtrl2', function ($scope) {
+    $scope.value = 84;
+    $scope.options = {
+      skin: {
+        type: 'tron',
+        width: 5,
+        color: '#494B52',
+        spaceWidth: 3
+      },
+      barColor: '#494B52',
+      trackWidth: 30,
+      barWidth: 30,
+      textColor: '#494B52'
+    };
+  })
+  .controller('knobCtrl3', function ($scope) {
+    $scope.value = 65;
+    $scope.options = {
+      unit: "%",
+      readOnly: true,
+      subText: {
+        enabled: true,
+        text: 'CPU used',
+        color: 'gray',
+        font: 'auto'
+      },
+      trackWidth: 40,
+      barWidth: 25,
+      trackColor: '#656D7F',
+      barColor: '#2CC185'
+    };
+  })
+  .controller('knobCtrl4', function ($scope) {
+    $scope.value = 70;
+    $scope.options = {
+      displayPrevious: true,
+      barCap: 25,
+      trackWidth: 30,
+      barWidth: 20,
+      trackColor: 'rgba(255,0,0,.1)',
+      prevBarColor: 'rgba(0,0,0,.2)',
+      textColor: 'rgba(255,0,0,.6)'
+    };
+  })
+  .controller('knobCtrl5', function ($scope) {
+    $scope.value = 85;
+    $scope.options = {
+      scale: {
+        enabled: true,
+        type: 'lines',
+        color: 'gray',
+        width: 1,
+        quantity: 20,
+        height: 8
+      },
+      trackWidth: 30,
+      barWidth: 30,
+      step: 5,
+      trackColor: 'rgba(52,152,219,.1)',
+      barColor: 'rgba(52,152,219,.5)'
+    };
+  })
+  .controller('knobCtrl6', function ($scope) {
+    $scope.value = 350;
+    $scope.options = {
+      min: -1000,
+      max: 1000,
+      barColor: '#5BC01E',
+      trackColor: '#212121',
+      trackWidth: 15,
+      barWidth: 30
+    };
+  })
+  .controller('knobCtrl7', function ($scope) {
+    $scope.value = 65;
+    $scope.options = {
+      scale: {
+        enabled: true,
+        type: 'dots',
+        color: 'rgba(255,0,0,.2)',
+        width: 2,
+        quantity: 50,
+        spaceWidth: 10
+      },
+      trackWidth: 25,
+      barWidth: 40,
+      trackColor: 'rgba(0,0,0,.1)'
+    };
+  })
+  .controller('knobCtrl8', function ($scope) {
+    $scope.value = 65;
+    $scope.options = {
+      displayInput: false,
+      animate: {
+        enabled: true,
+        duration: 2000,
+        ease: 'linear'
+      },
+      trackColor: 'rgba(33,33,33,.2)',
+      barColor: 'rgba(255,221,51,1)'
+    };
+  })
+  .controller('knobCtrl9', function ($scope) {
+    $scope.value = 65;
+    $scope.options = {
+      bgColor: '#2C3E50',
+      trackWidth: 50,
+      barWidth: 30,
+      barColor: '#FFAE1A',
+      textColor: '#eee'
+    };
+  })
+  .controller('knobCtrl10', function ($scope) {
+    $scope.value = 65;
+  })
+  .controller('knobCtrl11', function ($scope) {
+    $scope.value = 65;
   });
